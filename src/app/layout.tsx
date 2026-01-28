046a6bc9fcf755507ca426b3bfa3d8139427935c
Chandan2812 <chandanpms@gmail.com>
2026-01-28 18:06:30 +0530

    MS clarity tag updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -158,14 +158,14 @@ export default function RootLayout({
         </Script>
 
         {/* Microsoft Clarity */}
-        <Script id="clarity" strategy="afterInteractive">
+        <Script id="microsoft-clarity" strategy="afterInteractive">
           {`
-            (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r); t.async=1; t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "soeh75d1oz");
-          `}
+    (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "v8ibwvlg22");
+  `}
         </Script>
 
         {/* Google Tag Manager */}
