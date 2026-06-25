10a288fcac6445696a4706614588e03dc68fe1a9
lalit nath <lalitwebd@bigwigmediadigital.com>
2026-06-25 18:02:46 +0530

    admin panel date format

--- src/app/admin/leads/page.tsx
+++ src/app/admin/leads/page.tsx
@@ -265,7 +265,14 @@ const AdminLead = () => {
                     </td>
 
                     <td className="px-4 py-3">
-                      {new Date(contact.createdAt).toLocaleString()}
+                      {new Date(contact.createdAt).toLocaleString("en-GB", {
+                        day: "2-digit",
+                        month: "2-digit",
+                        year: "numeric",
+                        hour: "2-digit",
+                        minute: "2-digit",
+                        hour12: false,
+                      })}
                     </td>
 
                     <td className="px-4 py-3">
